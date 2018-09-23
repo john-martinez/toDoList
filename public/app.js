57636d6264107526cf9eeaa5b5e40525f847c318
John Martinez <jrvmartinz@gmail.com>
2018-09-23 18:19:23 +0000

    add update function

--- public/app.js
+++ public/app.js
@@ -1,13 +1,64 @@
 /* global $ */
 $(document).ready(function(){
     $.getJSON('/api/todos')
-    .then(addTodo)
+    .then(listTodos)
 });
-
-function addTodo(todos){
-    todos.forEach(function(todo){
-      var newTodo = $("<li class='task'>" + todo.name + "</li>");  
+function addTodo(todo){
+    var newTodo = $("<li class='task'>" + todo.name + " <span> X </span> </li>"); 
+    newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
       $('.list').append(newTodo);
+      if (todo.completed){
+          $('this').addClass('done');
+      }
+}
+
+
+function listTodos(todos){
+    todos.forEach(function(todo){
+      addTodo(todo);
     });
 
+}
+
+
+
+// ADD
+$('#todoInput').keypress(function(key){
+    if (key.which == 13){
+        var newData = {name: $('#todoInput').val() };
+        $.post('api/todos', newData)
+        .then(function(data){
+            console.log(data);
+            addTodo(data);
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+        $('#todoInput').val('');
+    }
+});
+
+// UPDATE
+$('.list').on('click', 'li', function(event){
+   updateTodo($(this));
+ 
+})
+
+function updateTodo(todo){
+    var isDone = todo.data('completed');
+    todo.data('completed', !isDone);
+    $.ajax({
+        method: "PUT",
+        url: '/api/todos/' + todo.data('id'),
+        data: {completed: !isDone}
+    })
+    .then(function(data){
+        console.log(data);
+         todo.toggleClass('done');
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+    
 }
